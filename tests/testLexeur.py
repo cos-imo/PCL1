b11ddba9119770086056af313950392bf826d989
LO <el-hadji-malick.lo@telecomnancy.eu>
2023-11-11 02:02:33 +0100

    Ajout d'un dossier test

---
+++ tests/testLexeur.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append('../source/token_analyser')
+sys.path.append('../source/token_generator')
+sys.path.append('../source/arguments')
+
+"""from token_analyser import *
+from token_generator import *
+from arguments import args"""
