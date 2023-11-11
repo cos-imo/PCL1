b6a4a4e6327927ff70ffdee21c7264d8723349ea
LO <el-hadji-malick.lo@telecomnancy.eu>
2023-11-11 21:14:01 +0100

    Suppression de l'ancien dossier tests

--- tests/testLexeur.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.append('../source/token_analyser')
-sys.path.append('../source/token_generator')
-sys.path.append('../source/arguments')
-
-"""from token_analyser import *
-from token_generator import *
-from arguments import args"""
