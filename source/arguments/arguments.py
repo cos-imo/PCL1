095cc85879fd1eebc463e7d8258e1dc07e5e74e5
UNGARO Cosimo <cosimo.ungaro@telecomnancy.eu>
2023-11-12 15:09:35 +0100

    [+]code: Ajout de modifications stash post-résolution. Ajout d'un mode dump

--- source/arguments/arguments.py
+++ source/arguments/arguments.py
@@ -13,6 +13,14 @@ class Parser:
         else:
             return option
 
+    def parse_dump_mode(self, option):
+        dump_mode={"all":"1", "token_liste":"2", "id_table":"3"}
+
+        if option in dump_mode:
+            return dump_mode[option]
+        else:
+            return option
+
     def parse_arguments(self):
         self.parser.add_argument('sourcefile', type=open)
 
@@ -20,6 +28,8 @@ class Parser:
         self.parser.add_argument('-v','--verbose', nargs='?', const="1", choices=["1", "2", "3"], type=self.parse_verbose_mode, help="Active le mode verbose (tout s'affiche)")
         self.parser.add_argument('-t', '--token-list', help="Affiche la liste des tokens générés (équivalent au second mode de verbose)")
         self.parser.add_argument('-i', '--table-identificateurs', help="Affiche la table des identificateurs (équivalent au troisième mode de verbose)")
+
+        self.parser.add_argument('-d', '--create-dump', nargs='?', const=1, choices=["1","2","3"], type=self.parse_dump_mode, help="Exporte les données dans un fichier")
 
         self.args = self.parser.parse_args()
 
