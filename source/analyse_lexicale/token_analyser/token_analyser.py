053150a6eb17457d66326b14f60960cf1a21621f
UNGARO Cosimo <cosimo.ungaro@telecomnancy.eu>
2023-11-13 21:44:27 +0100

    [~]code: Découpage de la nouvelle version du code

--- source/analyse_lexicale/token_analyser/token_analyser.py
+++ source/analyse_lexicale/token_analyser/token_analyser.py
@@ -11,13 +11,17 @@ class token_analyser_t():
         "true", "type", "use", "while", "with", ':', '(', ')', ',', ';', '=', '.', "'"
         ]
 
-    def analyse_token_compr(self,token):
-
-        if token == "" or token == "\n":
-            return (-1, token)
+    def codage_token(self, token):
+        if token == "\n":
+            return -1
+        
+        if token == '':
+            return None
         
         if token.isdigit():
-            return (7, token)
+            if token not in self.table_const:
+                self.table_const.append(token)
+            return (7, self.table_const.index(token) + 1)
 
         if token in self.mots:
             return self.mots.index(token) + 1
@@ -27,7 +31,6 @@ class token_analyser_t():
                 self.table_idf.append(token)
             return (6, self.table_idf.index(token) + 1)
 
-        # enfin si un caractère n'est pas reconnue : on renvoie un token d'erreur de valeur -1
+        # enfin si un caractère n'est pas reconnu : on renvoie un token d'erreur de valeur -1
         return (-1, token)
-
 
--- source/analyse_lexicale/token_generator/token_generator.py
+++ source/analyse_lexicale/token_generator/token_generator.py
@@ -1,81 +1,169 @@
 from token_analyser import * 
+import string
 
 class tokeniser_t:
 
     def __init__(self):
         self.liste_token=[]
         self.Analyser=token_analyser_t()
+        self.alphabet = string.ascii_lowercase + string.ascii_uppercase + "_"
+        self.analyser = token_analyser_t() 
 
-    def tokenise_file(self, file):
-        """Fonction qui prend en paramètre un fichier et qui renvoie une liste de tokens"""
+    def est_accepte(self, code):
+        """
+        ->param code : code à compiler
 
-        for line in file:
-            self.liste_token_ligne = []
-            ligne = list(line)
+        ->return : list_token, 
+                    numéro de ligne de fin de lecture ( permet de retourner la ligne ou il 
+        y a une erreur si il y en a une)
+                    True ou false suivant la réussite ou non de la lecture complète du code
+        """
+        token_courant = ''
+        peak_index = 0
+        ligne = 1
+        list_token = []
+        in_string = False
+        in_const = False
 
-            # On efface les commentaires
-            if '-' in ligne and ligne[ligne.index('-') + 1] == '-':
-                del ligne[ligne.index('-'):]
-            
-            # On va créer les tokens 
-            token = ""
-            in_string = False
-            for ind,lettre in enumerate(ligne):
-                if (lettre == " " or lettre  == "\n") and not in_string:
+        # on va lire caractère par caractère le code
+        while peak_index<len(code):
 
-                    # On est là dans le cas ou on a un token qui est fini et on est pas dans une chaine de caractères
-                    if token != "":
+            peak = code[peak_index]
 
-                        # C'est ici qu on va analyser le token et coder les unités lexicales
-                        tuple_token = self.Analyser.analyse_token_compr(token)
-                        self.liste_token_ligne.append(tuple_token)
+            # cas de fin de string
+            if in_string and peak != '"':
+                token_courant += peak
+                peak_index += 1
+                continue
 
-                        token = ""
-                else :
-                    if lettre == '"' and not in_string:
-                        # C'est le début d un chaîne de caractère
+            # cas de fin de constante
+            if in_const and not peak.isdigit() and peak != '.':
+                #cas de fin de constante
+                if token_courant not in self.table_const:
+                    self.table_const.append(token_courant)
+                list_token.append((7, self.table_const.index(token_courant) + 1))
+                token_courant = ''
+                in_const = False
 
-                        self.liste_token_ligne.append(token)
-                        token = ""
-                        in_string = True
-                        continue
+            # cas de commentaire
+            if peak == '-' and token_courant == '-' and not in_string:
+                #on ignire alors tout les caractères jusqu'à la fin de la ligne
+                while peak != '\n':
+                    peak_index += 1
+                    peak = code[peak_index]
+                ligne += 1
+                token_courant = ''
+                continue
 
-                    elif lettre == '"' and in_string:
-                        # c est la fin d une chaine de caractère
+                
+            if peak == '\n':
+                # si on est dans une chaine de caractrère on ajoute le \n au token courant 
+                # sinon on rajout juste le token courant à la liste des token et on ignore le caractère '\n'
+                if in_string:
+                    token_courant += peak
+                    peak_index += 1
+                    continue
+                ligne += 1
+                peak_index += 1
 
-                        in_string = False
-                        self.liste_token_ligne.append((11,token))
-                        token = ""
-                        continue
+                if token_courant != '':
+                    code_current_token = self.analyser.codage_token(token_courant)
+                    if code_current_token != -1:
+                        list_token.append(code_current_token)
+                    else:
+                        return list_token, ligne, False
+                    token_courant = ''
+                    continue
+
+            if peak == ' ' or peak == '\t':
+
+                # on garde les espaces dans les chaines de caractères
+                if in_string:
+                    token_courant += peak
+                    peak_index += 1
+                    continue
+
+
+                # cas d'esapce hors chaine de caractère, on ignore alors juste ce caractère
+
+                # cas de plmusieurs espaces à la suite
+                if token_courant == '':
+                    peak_index += 1
+                    continue
+                else:
+                    code_current_token = self.analyser.codage_token(token_courant)
+                    if code_current_token != -1 and code_current_token != None:
+                        list_token.append(code_current_token)
+                    peak_index += 1
+                    token_courant = ''
+                    continue
+
+            # on garde le point dans les constantes pour pouvoir prendre les floatants
+            if in_const :
+                if peak == '.' or peak.isdigit():
+                    token_courant += peak
+                    peak_index += 1
+                    continue
+
+            # cas de début de constante
+            if peak.isdigit():
+                token_courant += peak
+                peak_index += 1
+                in_const = True
+                continue
+
+
+            # cas de détection de caractères spéciaux
+            if peak in (';',',',':','(',')','.',"'",'"','+','-','*','/','=','>','<') :
+
+                # cas de détection de ':='
+                if peak == '=' and token_courant == ':'  and not in_string:
+                    token_courant += peak
+                    peak_index += 1
+                    list_token.append(5)
+                    token_courant = ''
+                    continue
+
+                # dans les autres cas le peak n'étant pas dans une chaine de caractère c est donc un séparateur et on place alors 
+                # le token courant dans la liste des token
+                code_current_token = self.analyser.codage_token(token_courant)
+                if code_current_token != -1 and code_current_token != None and not in_string:
+                    list_token.append(code_current_token)
+                    token_courant = ''
+
+                # si on a ':' on ne place pas dans la liste des token car cela oeut être un ':=' e vraie    
+                if peak == ':':
+                    token_courant += peak
+                    peak_index += 1
+                    continue
                     
-                    elif lettre in (';',',',':','(',')','.',"'") and not in_string:
-                        # On doit vérifier si on a := ou juste : 
-
-                        if lettre == ':' and ligne[ind+1] == '=':
-                            token += lettre
-                            continue
-
-                        if token !='':
-                            self.liste_token_ligne.append(self.Analyser.analyse_token_compr(token))
-                        self.liste_token_ligne.append(self.Analyser.analyse_token_compr(lettre))
-                        token = ""
+                # cas de début d 'une chaine de caractère    
+                if peak == '"':
+                    if in_string:
+                        list_token.append(self.analyser.codage_token(token_courant))
+                        token_courant = ''
+                        in_string = False
+                        peak_index += 1
                         continue
-                    token += lettre
-            if self.liste_token_ligne != []:
-                self.liste_token.append(self.liste_token_ligne)
-           
-        self.liste_token = [element for sousliste in self.liste_token for element in (sousliste if isinstance(sousliste, list) else [sousliste])]
+                    else:
+                        token_courant = ''
+                        in_string = True
+                        peak_index += 1
+                        continue
+                
+                list_token.append(self.analyser.codage_token(peak))
+                token_courant = ''
+                peak_index += 1
+                continue
+            
+            if peak in self.alphabet:
+                token_courant += peak
+                peak_index += 1
+                continue
 
-        return self.liste_token
- 
-    def verbose(self, file, verbose_mode):
-        print("mode verbose: activé")
-        token_liste = tokeniser_t.tokenise_file(self, file)
-        if verbose_mode in ["1", "2"]: 
-            print(" === LISTE DES TOKENS === ")
-            print(token_liste)
-        if verbose_mode in ["1","3"]:
-            print(" === TABLE D'IDENTIFICATION === ")
-            print(self.Analyser.table_idf)
+            else:
+                peak_index += 1
 
-
+        list_token.append(self.analyser.codage_token(token_courant))
+        self.liste_token = list_token
+        return list_token[:-1], ligne, True
