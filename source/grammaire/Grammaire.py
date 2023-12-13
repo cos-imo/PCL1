9da7db41328a425320c662b8f98b32c7a7de2523
Alex ZHOU <alex.zhou@telecomnancy.eu>
2023-12-13 13:08:34 +0100

    Rattrapage de l'erreur en cas de fichier non présent

--- source/grammaire/Grammaire.py
+++ source/grammaire/Grammaire.py
@@ -5,11 +5,15 @@ class Grammaire:
 	def __init__(self):
 		print("Grammaire initialisée")
 
-    def charger_grammaire(self):
-        with open("input.gramm","r") as file:
-            data = file.readlines()
-            grammaire_brute = [element.replace("\n","") for element in data]
-            return grammaire_brute
+	def charger_grammaire(self):
+	        try:
+	            with open("input.gramm","r") as file:
+	                data = file.readlines()
+	                grammaire_brute = [element.replace("\n","") for element in data]
+	                return grammaire_brute
+	        except:
+	            sys.stdout.write("Warning: Fichier input.gramm non trouvé.\nVoulez-vous tenter d'ouvrir un autre fichier?\n\nOuverture d'un autre fichier impossible dans l'état 	actuel des choses, désolé\n")
+	            exit()
 
 if __name__=="__main__":
 	grammaire = Grammaire()
