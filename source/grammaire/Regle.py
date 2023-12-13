c9513e5864d59c118cd78cd2587614821786cfdd
Alex ZHOU <alex.zhou@telecomnancy.eu>
2023-12-13 17:10:49 +0100

    Les premiers sont désormais calculés depuis une string

--- source/grammaire/Regle.py
+++ source/grammaire/Regle.py
@@ -4,11 +4,14 @@ import RegleManager
 
 class Regle:
 
-	def __init__(self, indice, expression):
+	def __init__(self, indice, expression, premier_terminaux):
 		self.indice = indice+1
+		
 		self.membre_gauche = None
 		self.membre_droit = None
+		
 		self.raw_regle = expression
+
 		self.non_terminaux = []
 		self.terminaux = []
 
@@ -16,6 +19,12 @@ class Regle:
 		self.membre_droit = self.decoupe_membre_droit()
 
 		self.regle_decoupee = self.decoupe(expression)
+
+		self.premiers_terminaux = premier_terminaux
+		self.premiers = []
+		self.suivants = []
+
+		#self.ajouter_premier()
 
 		self.initialiser_non_terminaux()
 		self.initialiser_terminaux()
@@ -39,6 +48,21 @@ class Regle:
 
 	def decoupe_membre_gauche(self):
 		return self.raw_regle.split("->")[0]
-
+	
+	"""
+	def ajouter_premier(self):
+		if self.membre_droit[0].islower():
+			self.premiers.append(self.membre_droit[0])
+		else:
+			if self.membre_droit[0] in self.premiers_terminaux:
+				self.premiers.append(self.premiers_terminaux[self.membre_droit[0]])
+			else:
+				print("Erreur: premier non trouvé : " + self.raw_regle)
+				exit()
+	"""
+				
 	def __repr__(self):
-		return "Regle\n\n\tExpresion: " + self.raw_regle + "\n\tPremiers: \n"
+		premiers_str = ""
+		for premier in self.premiers:
+			premiers_str += "\t" + premier
+		return "Regle\n\n\tExpresion: " + self.raw_regle + "\n\tPremiers: \n" + premiers_str
--- source/grammaire/RegleManager.py
+++ source/grammaire/RegleManager.py
@@ -7,16 +7,18 @@ class RegleManager:
 
 	def __init__(self, ensemble_de_regles):
 		self.ensemble_raw = ensemble_de_regles
+
+		self.premiers_terminaux = {}
+		self.suivants = {}
+
 		self.ensemble_regles = []
 		self.initialiser_regles()
-		self.premiers = {}
-		self.suivants = {}
-		self.ajouter_premier()
-		print(self.premiers)
+		self.ajouter_premier_terminaux()
+		print(self.premiers_terminaux)
 	
 	def ajouter_regle(self, expression):
 		if self.ensemble_regles == []:
-			regle = Regle.Regle(0, expression)
+			regle = Regle.Regle(0, expression, self.premiers_terminaux)
 			self.ensemble_regles.append(regle)
 			self.ensemble_regles = list(set(self.ensemble_regles))
 			return
@@ -25,34 +27,46 @@ class RegleManager:
 				print("Doublon trouvé: " + expression)
 				return
 			else:
-				regle = Regle.Regle(len(self.ensemble_regles), expression)
+				regle = Regle.Regle(len(self.ensemble_regles), expression, self.premiers_terminaux)
 				self.ensemble_regles.append(regle)
 				self.ensemble_regles = list(set(self.ensemble_regles))
 				return
 	
-	def ajouter_premier(self):
+	def ajouter_premier_terminaux(self):
 		queue = []
-		for regle in self.ensemble_regles:
-			if regle.membre_droit[0].islower():
-				if regle.membre_gauche.replace(" ","") not in self.premiers:
-					self.premiers[regle.membre_gauche.replace(" ","")] = regle.membre_droit[0].replace(" ","")
+		for regle in self.ensemble_raw:
+			partie_droite = regle.split("->")[1][1:].split(" ")
+			partie_gauche = regle.split("->")[0].replace(" ","")
+			if partie_droite[0].islower():
+				if partie_gauche not in self.premiers_terminaux:
+					self.premiers_terminaux[partie_gauche] = partie_droite[0]
 				else:
-					self.premiers[regle.membre_gauche.replace(" ","")] += regle.membre_droit[0].replace(" ","")
+					self.premiers_terminaux[partie_gauche] += partie_droite[0]
 			else:
-				if regle.membre_droit[0] in self.premiers:
-					if regle.membre_gauche.replace(" ","") not in self.premiers:
-						self.premiers[regle.membre_gauche.replace(" ","")] = self.premiers[regle.membre_droit[0].replace(" ","")]
+				if partie_droite[0] in self.premiers_terminaux:
+					if regle.partie_gauche not in self.premiers_terminaux:
+						self.premiers_terminaux[partie_gauche] = self.premiers_terminaux[partie_droite[0]]
 					else:
-						self.premiers[regle.membre_gauche.replace(" ","")] += self.premiers[regle.membre_droit[0].replace(" ","")]
+						self.premiers_terminaux[partie_gauche] += self.premiers_terminaux[partie_droite[0]]
 				else:
 					queue.append(regle)
 
+		for i in range(len(queue)):
+			for element in queue:
+				partie_droite = element.split("->")[1][1:].split(" ")
+				partie_gauche = element.split("->")[0].replace(" ","")
+				if partie_droite[0] in self.premiers_terminaux:
+					self.premiers_terminaux[partie_gauche] = self.premiers_terminaux[partie_droite[0]]
+					queue.remove(element)
 		for element in queue:
-			if regle.membre_droit[0] in self.premiers:
-				self.premiers[element.membre_gauche.replace(" ","")] = self.premiers[regle.membre_droit[0].replace(" ","")]
+			partie_droite = element.split("->")[1][1:].split(" ")
+			partie_gauche = element.split("->")[0].replace(" ","")
+			if partie_droite[0] in self.premiers_terminaux:
+				self.premiers_terminaux[partie_gauche] = self.premiers_terminaux[partie_droite[0]]
 				queue.remove(element)
 			else:
-				print("Erreur: " + regle.membre_gauche + " n'a pas de premiers.\n Vérifiez votre grammaire.")
+				print("Erreur: " + partie_gauche + " n'a pas de premiers.\n Vérifiez votre grammaire.")
+
 			
 	
 	def initialiser_regles(self):
