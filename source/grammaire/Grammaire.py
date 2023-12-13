207095e2422d9bbf37e40160b711b6b301350c6c
Alex ZHOU <alex.zhou@telecomnancy.eu>
2023-12-13 13:39:00 +0100

    Déplacement de la création des terminaux et non terminaux dans Regle

--- source/grammaire/Grammaire.py
+++ source/grammaire/Grammaire.py
@@ -4,13 +4,6 @@ import Regle
 class Grammaire:
 	def __init__(self):
 		self.grammaire_brute = self.charger_grammaire()
-		self.grammaire_decoupee = self.decoupe(self.grammaire_brute)
-		self.non_terminaux = []
-		self.terminaux = []
-		self.initialiser_non_terminaux()
-		self.initialiser_terminaux()
-		print(self.non_terminaux)
-		print(self.terminaux)
 
 	def charger_grammaire(self):
 	        try:
@@ -21,24 +14,6 @@ class Grammaire:
 	        except:
 	            sys.stdout.write("Warning: Fichier input.gramm non trouvé.\nVoulez-vous tenter d'ouvrir un autre fichier?\n\nOuverture d'un autre fichier impossible dans l'état 	actuel des choses, désolé\n")
 	            exit()
-	            
-	def decoupe(self, grammaire_brute):
-		grammaire_decoupee = []
-		for line in grammaire_brute:
-			grammaire_decoupee.append(line.split(" -> "))
-		return grammaire_decoupee
-
-	def initialiser_non_terminaux(self):
-		for line in self.grammaire_decoupee:
-			self.non_terminaux.append(line[0])
-			self.non_terminaux += [char for char in line[1].split(" ") if char.isupper()]
-			self.non_terminaux = list(set(self.non_terminaux))
-
-	def initialiser_terminaux(self):
-		for line in self.grammaire_decoupee:
-			self.terminaux += [char for char in line[1].split(" ") if char.islower()]
-			self.terminaux = list(set(self.terminaux))
-
 
 if __name__=="__main__":
 	grammaire = Grammaire()
--- source/grammaire/Regle.py
+++ source/grammaire/Regle.py
@@ -1,6 +1,32 @@
 import sys
+import math
 
 class Regle:
 
-    def __init__(self):
-        sys.stdout.write("Création de la règle\n")
+	def __init__(self):
+		self.indice = math.inf
+		self.membre_gauche = None
+		self.membre_droit = None
+		self.raw_regle = ""
+		self.non_terminaux = []
+		self.terminaux = []
+		self.initialiser_non_terminaux()
+		self.initialiser_terminaux()
+		sys.stdout.write("Création de la règle\n")
+		
+	def decoupe(self, grammaire_brute):
+		grammaire_decoupee = []
+		for line in grammaire_brute:
+			grammaire_decoupee.append(line.split(" -> "))
+		return grammaire_decoupee
+
+	def initialiser_non_terminaux(self):
+		for line in self.grammaire_decoupee:
+			self.non_terminaux.append(line[0])
+			self.non_terminaux += [char for char in line[1].split(" ") if char.isupper()]
+			self.non_terminaux = list(set(self.non_terminaux))
+
+	def initialiser_terminaux(self):
+		for line in self.grammaire_decoupee:
+			self.terminaux += [char for char in line[1].split(" ") if char.islower()]
+			self.terminaux = list(set(self.terminaux))
