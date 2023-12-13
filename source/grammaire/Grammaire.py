15d8bc59245b8b7828ebb9faf6b20242d561e5a9
Alex ZHOU <alex.zhou@telecomnancy.eu>
2023-12-13 13:05:34 +0100

    Ajout de l'ouverture d'un fichier

--- source/grammaire/Grammaire.py
+++ source/grammaire/Grammaire.py
@@ -5,5 +5,11 @@ class Grammaire:
 	def __init__(self):
 		print("Grammaire initialisée")
 
+    def charger_grammaire(self):
+        with open("input.gramm","r") as file:
+            data = file.readlines()
+            grammaire_brute = [element.replace("\n","") for element in data]
+            return grammaire_brute
+
 if __name__=="__main__":
 	grammaire = Grammaire()
