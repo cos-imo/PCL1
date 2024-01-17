9cef1fd33b42ccb0d4512ff1e8844f083a07952b
LO <el-hadji-malick.lo@telecomnancy.eu>
2024-01-17 07:12:41 +0100

    Premiers tests

--- source/modules/arbre/arbre.py
+++ source/modules/arbre/arbre.py
@@ -1,3 +1,10 @@
 import graphviz
 
 graph = graphviz.Digraph()
+
+graph.node('A', 'fichier')  # doctest: +NO_EXE
+graph.node('B', 'decl')
+
+graph.edge('A','B', 'with Ada.Text_IO; use Ada.Text_IO \n procedure <pro> is is <decl> \n begin <instr>end <ident> $')
+
+graph.render('L\'arbre syntaxique', view=True)
