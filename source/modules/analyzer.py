83abc0dd2a89f85f72afc4eb598a50a3d75e5137
UNGARO Cosimo <cosimo.ungaro@telecomnancy.eu>
2024-01-17 17:25:53 +0100

    [chore] Pour que mes camarades puissent suivre :)

--- source/modules/analyzer.py
+++ source/modules/analyzer.py
@@ -5,30 +5,47 @@ def parse_syntax(automate, grammaire):
 
     liste_token = automate.liste_token
 
+    print(liste_token)
+    
     positionStream = 0
     stack = [grammaire.axiomeRegle.get_membre_gauche()]
+    
+    print(grammaire.terminaux)
 
     while stack:
         current_symbol = stack.pop()
 
+
         if isinstance(current_symbol, tuple):
             continue
 
-        if current_symbol in grammaire.terminaux:
+        if current_symbol in grammaire.token_terminaux:
             if current_symbol == liste_token[positionStream]:
+                print(current_symbol)
                 positionStream += 1
             else:
                 print(f"Erreur: Prévu '{current_symbol}', recu '{liste_token[positionStream]}'")
                 return False
 
         elif current_symbol in grammaire.non_terminaux:
-            rule_key = liste_token[positionStream]
-            if rule_key in grammaire.regle_by_premier[current_symbol]:
-                production_rule = grammaire.regle_by_premier[current_symbol][rule_key][0]
-                production_right = production_rule.get_membre_droit()
-                stack.extend(production_right[::-1])
+            print(current_symbol)
+            listeRegleAjout = grammaire.regleByNonTerminal[current_symbol]
+            for regle in listeRegleAjout:
+                if regle.get_membre_droit()[0] == liste_token[positionStream]:
+                    RegleAjout = regle
+            stack.extend(regle.RegleInt[::-1])
+            # rule_key = liste_token[positionStream]
+            # if rule_key in grammaire.premiersDico[current_symbol]:
+            #     production_rule = regleAjout[rule_key][0]
+            #     production_right = production_rule.get_membre_droit()
+            #     stack.extend(production_right[::-1])
+            #     print(stack)
+            # else:
+            #     print(f"ERREUR: Règle non trouvée pour {current_symbol}, token: {liste_token[positionStream]}")
+
 
         elif current_symbol == '$':
+            print("Fin atteinte")
             if positionStream == len(liste_token):
                 print("Parsing réussi!")
                 return True
